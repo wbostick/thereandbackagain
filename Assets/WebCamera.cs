0b6ea6ae4d07a61132bf2c92e1f421c58c27ebcf
George Castle <voxlap@gmail.com>
2021-10-15 21:00:11 -0400

    Imported packages

---
+++ Assets/WebCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamera : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture myCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("no camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (devices[i].isFrontFacing)
+            {
+                myCam = new WebCamTexture(devices[i].name);
+                     }
+        }
+
+        if (myCam == null)
+        { return;
+        }
+
+        myCam.Play();
+        background.texture = myCam;
+
+        camAvailable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!camAvailable)
+            return;
+
+       
+
+    }
+}
